94f3ea6be589b47ba039eff1bd75ce224ec36669
Chek97 <felychk203@hotmail.com>
2023-03-04 03:52:18 -0500

    creada pagina sobre mi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log("Iniciando");
+
+function generateText() {
+    const text = document.getElementById("about-text");
+    text.innerHTML += "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod odit labore totam nesciunt, ipsa nemo nam fugiat dignissimos amet itaque perferendis, quibusdam nostrum, culpa cupiditate eius quas commodi harum voluptatibus.";
+}
